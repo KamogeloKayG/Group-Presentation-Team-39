756deffa6259713101a771afbd8bafda64e204ad
Mahlako Mogoane <mahlakomogoane@outlook.com>
2022-09-08 14:43:25 +0200

    store user age and gender and calculate BMI for profile

--- Form1.vb
+++ Form1.vb
@@ -1,3 +1,10 @@
 ﻿Public Class frmScreening
+    Private user As user
+    Private weight, height As Double
 
+    Private Sub btnEnter_Click(sender As Object, e As EventArgs) Handles btnEnter.Click
+        user.age = CInt(txtAge.Text)
+        user.gender = cboGender.Text
+        user.bmi = weight / height ^ 2
+    End Sub
 End Class
---
+++ user.vb
@@ -0,0 +1,42 @@
+﻿Public Class user
+    Private _age As Integer
+    Private _bmi As Double
+    'Private _smoker, _alcoholic As Boolean
+    Private _gender As String
+
+    'constructor
+    Public Sub New()
+
+    End Sub
+
+    'property
+    Public Property age As Double
+        Get
+            Return _age
+        End Get
+        Set(value As Double)
+            _age = value
+        End Set
+    End Property
+
+    Public Property bmi As Double
+        Get
+            Return _bmi
+        End Get
+        Set(value As Double)
+            _bmi = value
+        End Set
+    End Property
+
+    Public Property gender As String
+        Get
+            Return _gender
+        End Get
+        Set(value As String)
+            _gender = value
+        End Set
+    End Property
+
+    'method
+    'utility
+End Class
