af5b2557b6377c000d9fd57bf7181d9d0b9266ce
MaposaTavonga <112952896+MaposaTavonga@users.noreply.github.com>
2022-09-13 13:30:16 +0200

    Update Viral.vb

--- Viral.vb
+++ Viral.vb
@@ -5,7 +5,55 @@ Option Infer Off
 Public Class Viral
 
     Private _report As report
+    Private _age As Integer
+    Private _gender As Boolean
+    Private _sexualActivity As Integer
+    Private _drugintake As Integer
+    Private _chances As Integer
+    Public Property age As Integer
+        Get
+            Return _age
+        End Get
+        Set(value As Integer)
+            _age = value
+        End Set
+    End Property
+    Public Property gender As Boolean
+        Get
+            Return _gender
 
+        End Get
+        Set(value As Boolean)
+            _gender = value
+        End Set
+    End Property
+    Public Property SexualActivity As Integer
+        Get
+            Return _sexualActivity
+        End Get
+        Set(value As Integer)
+            _sexualActivity = value
+        End Set
+    End Property
+
+
+    Public Property Drugs As Integer
+        Get
+            Return _drugintake
+        End Get
+        Set(value As Integer)
+            _drugintake = value
+        End Set
+    End Property
+
+    Public Function Chnces() As Integer
+        If _sexualActivity > 10 Then
+            _chances = 50%
+        Else
+            _chances = 0
+        End If
+        Return _chances
+    End Function
 
     Public Property report As report
         Get
